c0899445b9055a494ebc2d45678d9aa54889ffea
dmasrii <dmasri@udesa.edu.ar>
2024-06-17 23:44:11 -0300

    archivo saludo terminado

---
+++ js/archivoSaludo.js
@@ -0,0 +1,15 @@
+let recuperoStorage2 = localStorage.getItem ('email')
+let recuperoStorage3 = localStorage.getItem ('contra')
+
+if (recuperoStorage2 != null){
+    let mensajeSaludo = `Bienvenido: ${recuperoStorage2}`
+    let mostrarMensaje2 = document.querySelector ('.mensajeSaludo')
+    mostrarMensaje2.innerText = mensajeSaludo
+    let logout = document.querySelector ('.header2')
+    logout.innerHTML += '<a href="./js.logout.js">Log Out</a>'
+    let desaparece = document.querySelector ('.saludoli')
+    let desaparece2 = document.querySelector ('.saludoli2')
+    desaparece.style.display = 'none'
+    desaparece2.style.display ='none'
+}
+
